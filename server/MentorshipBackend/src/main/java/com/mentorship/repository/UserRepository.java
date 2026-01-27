e7093b02d72965b0a695fccc7cf770d8e750202f
Arvin Bhat <arvinbhat.ab@gmail.com>
2026-01-27 08:52:31 +0530

    User controller updated and repostory added

--- server/MentorshipBackend/src/main/java/com/mentorship/repository/UserRepository.java
+++ server/MentorshipBackend/src/main/java/com/mentorship/repository/UserRepository.java
@@ -1,9 +1,17 @@
 package com.mentorship.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.mentorship.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+	Optional<User> findByEmail(String email);
+	
+	// derived finder
+		boolean existsByEmail(String email);
+
 
 }
