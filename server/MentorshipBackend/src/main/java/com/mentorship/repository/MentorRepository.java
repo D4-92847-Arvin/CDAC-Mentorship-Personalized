de8f37a74d27f2ef6f2406a86499822b401d0458
Arvin Bhat <arvinbhat.ab@gmail.com>
2026-01-28 17:11:17 +0530

    Resolve merge conflict and fix backend authentication issues

--- server/MentorshipBackend/src/main/java/com/mentorship/repository/MentorRepository.java
+++ server/MentorshipBackend/src/main/java/com/mentorship/repository/MentorRepository.java
@@ -1,9 +1,16 @@
 package com.mentorship.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.mentorship.entities.Mentor;
+import com.mentorship.entities.VerificationStatus;
 
 public interface MentorRepository extends JpaRepository<Mentor, Long> {
+	 List<Mentor> findByVerificationStatus(VerificationStatus status);
 
+	    // Get verified mentors by specialization (case-insensitive, partial match)
+	    List<Mentor> findByVerificationStatusAndSpecializationContainingIgnoreCase(
+	            VerificationStatus status,String specialization);
 }
